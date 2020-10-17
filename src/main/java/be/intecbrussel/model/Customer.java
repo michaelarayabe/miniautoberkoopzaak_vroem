5f8462c44dfe352bf163b645e6c935d6f0dd4704
michaelarayabe <michaelseifuaraya@gmail.com>
2020-10-17 12:30:41 +0200

    added dependencies such as javax persistence api, mysql connector and  hibernate. created model classes Employee and Customer created persistence.xml

---
+++ src/main/java/be/intecbrussel/model/Customer.java
@@ -0,0 +1,175 @@
+package be.intecbrussel.model;
+
+import java.util.Objects;
+
+public class Customer {
+
+    private int customerNumber;
+    private String customerName;
+    private String contactLastName;
+    private String contactFirstName;
+    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private double creditLimit;
+
+    public Customer() {
+    }
+
+    public Customer(int customerNumber, String customerName, String contactLastName, String contactFirstName, String phone, String addressLine1, String addressLine2, String city, String state, String postalCode, String country, double creditLimit) {
+        this.customerNumber = customerNumber;
+        this.customerName = customerName;
+        this.contactLastName = contactLastName;
+        this.contactFirstName = contactFirstName;
+        this.phone = phone;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.creditLimit = creditLimit;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getContactLastName() {
+        return contactLastName;
+    }
+
+    public void setContactLastName(String contactLastName) {
+        this.contactLastName = contactLastName;
+    }
+
+    public String getContactFirstName() {
+        return contactFirstName;
+    }
+
+    public void setContactFirstName(String contactFirstName) {
+        this.contactFirstName = contactFirstName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerNumber=" + customerNumber +
+                ", customerName='" + customerName + '\'' +
+                ", contactLastName='" + contactLastName + '\'' +
+                ", contactFirstName='" + contactFirstName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", addressLine1='" + addressLine1 + '\'' +
+                ", addressLine2='" + addressLine2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                ", creditLimit=" + creditLimit +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customerNumber == customer.customerNumber &&
+                Double.compare(customer.creditLimit, creditLimit) == 0 &&
+                Objects.equals(customerName, customer.customerName) &&
+                Objects.equals(contactLastName, customer.contactLastName) &&
+                Objects.equals(contactFirstName, customer.contactFirstName) &&
+                Objects.equals(phone, customer.phone) &&
+                Objects.equals(addressLine1, customer.addressLine1) &&
+                Objects.equals(addressLine2, customer.addressLine2) &&
+                Objects.equals(city, customer.city) &&
+                Objects.equals(state, customer.state) &&
+                Objects.equals(postalCode, customer.postalCode) &&
+                Objects.equals(country, customer.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerNumber, customerName, contactLastName, contactFirstName, phone, addressLine1, addressLine2, city, state, postalCode, country, creditLimit);
+    }
+}
---
+++ src/main/java/be/intecbrussel/model/Employee.java
@@ -0,0 +1,103 @@
+package be.intecbrussel.model;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private int employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String extension;
+    private String email;
+    private String jobTitle;
+
+    public Employee() {
+    }
+
+    public Employee(int employeeNumber, String lastName, String firstName, String extension, String email, String jobTitle) {
+        this.employeeNumber = employeeNumber;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.extension = extension;
+        this.email = email;
+        this.jobTitle = jobTitle;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeNumber=" + employeeNumber +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", extension='" + extension + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return employeeNumber == employee.employeeNumber &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(extension, employee.extension) &&
+                Objects.equals(email, employee.email) &&
+                Objects.equals(jobTitle, employee.jobTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeNumber, lastName, firstName, extension, email, jobTitle);
+    }
+}
